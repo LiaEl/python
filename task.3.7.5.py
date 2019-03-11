935c5ff565781c6d4a53e57700ce358573b0f759
Julia <alialialar@gmail.com>
2019-03-11 15:10:28 +0300

    Create task.3.7.5.py

---
+++ task.3.7.5.py
@@ -0,0 +1,17 @@
+l = []
+
+d = {}
+for i in range(1, 12):
+    d[i] = [0, 0]
+
+with open('C:\\Users\\User\\Documents\\python\\data375.txt') as fi:
+    for line in fi:
+        b = line.strip().split()
+        d[int(b[0])][0] += float(b[2])
+        d[int(b[0])][1] += 1
+
+for i in d.keys():
+    if d[i][1] == 0:
+        print(i, '-')
+    else:
+        print(i, d[i][0] / d[i][1])
