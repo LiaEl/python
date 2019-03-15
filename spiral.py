9e1f94503bfa5b1c7582e4e8a3fecf3aa8170ebf
Iuliia Laricheva <alialialar@gmail.com>
2019-03-15 15:41:07 +0200

    Solution for Spiral task Выведите таблицу размером n×n, заполненную числами от 1 до n2 по спирали, выходящей из левого верхнего угла и закрученной по часовой  стрелке, как показано в примере (здесь n=5):

---
+++ spiral.py
@@ -0,0 +1,23 @@
+n = int(input())
+
+x, y = 0, 0 # base coordinates
+step_x, step_y = 0, 1 # steps for X and Y
+spiral = [[None] * n for _ in range(n)] # empty array
+
+for i in range(1, n ** 2 + 1):
+    spiral[x][y] = i # fill the array
+    next_x, next_y = x + step_x, y + step_y # create next cell
+
+    if 0 <= next_x < n and 0 <= next_y < n and spiral[next_x][next_y] == None: # check next cell is exist and empty
+        x += step_x
+        y += step_y
+
+    else:
+        step_x, step_y = step_y, -step_x # change direction
+        x += step_x
+        y += step_y
+
+for x in range(n):
+    for y in range(n):
+        print(spiral[x][y], end=' ')
+    print()
